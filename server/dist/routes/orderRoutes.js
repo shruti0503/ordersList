300a7ae41606149cc06c7fd8f5c0dcd0b7fbc1cc
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-09-17 00:53:25 +0530

    d

--- server/dist/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const Orders_1 = require("../controllers/Orders");
-router.get("/get-orders", Orders_1.getOrders);
-exports.default = router;
